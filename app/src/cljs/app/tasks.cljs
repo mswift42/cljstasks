ccd0b70436be609fb0c8100cd0e19e079e39ba9c
martin.haesler <martin.haesler@gmail.com>
2015-07-13 18:42:09 +0100

    add add button html on handler.

--- app/src/cljs/app/tasks.cljs
+++ app/src/cljs/app/tasks.cljs
@@ -6,6 +6,11 @@
             [{:summary "task1"}
              {:summary "task2"}]))
 
+(defn add-task
+  "add a task to var tasks"
+  [task]
+  (swap! tasks conj task))
+
 (defn single-task
   "single-task returns a html element for a given task"
   [task]
@@ -17,15 +22,14 @@
   "list-tasks is a component container for a list
    of single-tasks."
   []
-  [:div.addtask
-   [:button.mdl-button.mdl-js-button]]
+  
   [:div.tasklist
+   [:div.addtask
+   [:button.mdl-button.mdl-js-button.mdl-button--fab.mdl-js-ripple-effect.mdl-button--colored {:on-click #(add-task {:summary ""})}
+    [:i.material-icons "add"]]]
    [:ul
     (for [i @tasks]
       ^{:key i}
       (single-task i))]])
 
-(defn add-task
-  "add a task to var tasks"
-  [task]
-  (swap! tasks conj task))
+
